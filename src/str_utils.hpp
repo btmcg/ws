a583cf2d5a1d28d9e4de9fd6403a78d8cdea1bd4
brian <brian@logos>
2025-07-17 11:19:29 -0400

    formatting

--- src/str_utils.hpp
+++ src/str_utils.hpp
@@ -6,51 +6,51 @@
 #include <vector>
 
 namespace ws {
-    void
-    ltrim(std::string& s)
-    {
-        s.erase(s.begin(),
-                std::find_if(s.begin(), s.end(), [](std::uint8_t c) { return !std::isspace(c); }));
-    }
+void
+ltrim(std::string& s)
+{
+    s.erase(s.begin(),
+            std::find_if(s.begin(), s.end(), [](std::uint8_t c) { return !std::isspace(c); }));
+}
 
-    // Function to trim trailing whitespace
-    void
-    rtrim(std::string& s)
-    {
-        s.erase(std::find_if(s.rbegin(), s.rend(), [](std::uint8_t c) { return !std::isspace(c); })
-                        .base(),
-                s.end());
-    }
+// Function to trim trailing whitespace
+void
+rtrim(std::string& s)
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](std::uint8_t c) { return !std::isspace(c); })
+                    .base(),
+            s.end());
+}
 
-    void
-    trim(std::string& s)
-    {
-        ltrim(s);
-        rtrim(s);
-    }
+void
+trim(std::string& s)
+{
+    ltrim(s);
+    rtrim(s);
+}
 
-    std::vector<std::string>
-    tokenize(std::string const& str)
-    {
-        std::vector<std::string> tokens;
-        auto itr = str.begin();
+std::vector<std::string>
+tokenize(std::string const& str)
+{
+    std::vector<std::string> tokens;
+    auto itr = str.begin();
 
-        while (itr != str.end()) {
-            // skip leading whitespace
-            itr = std::find_if_not(itr, str.end(), [](std::uint8_t c) { return std::isspace(c); });
+    while (itr != str.end()) {
+        // skip leading whitespace
+        itr = std::find_if_not(itr, str.end(), [](std::uint8_t c) { return std::isspace(c); });
 
-            // find end of word
-            auto word_end
-                    = std::find_if(itr, str.end(), [](std::uint8_t c) { return std::isspace(c); });
+        // find end of word
+        auto word_end
+                = std::find_if(itr, str.end(), [](std::uint8_t c) { return std::isspace(c); });
 
-            // if a non-empty word is found, add it to tokens
-            if (itr != word_end) {
-                tokens.emplace_back(itr, word_end);
-            }
-
-            itr = word_end; // move iterator past the current word/whitespace
+        // if a non-empty word is found, add it to tokens
+        if (itr != word_end) {
+            tokens.emplace_back(itr, word_end);
         }
-        return tokens;
+
+        itr = word_end; // move iterator past the current word/whitespace
     }
+    return tokens;
+}
 
 } // namespace ws
--- src/tcp_echo_server/tcp_echo_server.hpp
+++ src/tcp_echo_server/tcp_echo_server.hpp
@@ -4,20 +4,20 @@
 
 namespace ws {
 
-    class tcp_echo_server
-    {
-    public:
-        tcp_echo_server(int port);
-        ~tcp_echo_server() noexcept;
-        bool listen();
+class tcp_echo_server
+{
+public:
+    tcp_echo_server(int port);
+    ~tcp_echo_server() noexcept;
+    bool listen();
 
-    private:
-        void parse_request(std::string const&);
-        bool parse_method(std::string const&);
+private:
+    void parse_request(std::string const&);
+    bool parse_method(std::string const&);
 
-    private:
-        int sock_ = -1;
-        int port_ = -1;
-    };
+private:
+    int sock_ = -1;
+    int port_ = -1;
+};
 
 } // namespace ws
