b0a963b006cc0ce34259a0ee3609d3c48cb69650
brian <brian@logos>
2025-07-18 15:23:53 -0400

    outline websocket_frame

--- src/websocket_frame.hpp
+++ src/websocket_frame.hpp
@@ -17,9 +17,31 @@ enum class op_code : std::uint8_t
 
 struct websocket_frame
 {
-    bool fin;
-    op_code op;
-    std::vector<std::uint8_t> payload;
-};
+    bool fin : 1;
+    std::uint8_t rsv1 : 1;
+    std::uint8_t rsv2 : 1;
+    std::uint8_t rsv3 : 1;
+    op_code op : 4;
+
+    std::uint8_t mask : 1;
+    std::uint8_t payload_len : 7;
+
+    std::uint16_t extended_payload_len_16; ///< used when payload_len == 126
+    std::uint64_t extended_payload_len_64; ///< used when payload_len == 127
+
+    std::uint8_t masking_key[4];
+
+    std::uint64_t
+    get_payload_len() const noexcept
+    {
+        if (payload_len < 126) {
+            return payload_len;
+        }
+        if (payload_len == 126) {
+            return std::byteswap(extended_payload_len_16);
+        }
+        return std::byteswap(extended_payload_len_64);
+    }
+} __attribute__((packed));
 
 } // namespace ws
